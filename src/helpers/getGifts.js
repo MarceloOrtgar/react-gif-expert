69cbc7313025a4a3a03c31f2bcafa3fba9b7e264
Marcelo <marceloortega18@gmail.com>
2022-09-13 14:43:59 -0400

    primer commit

---
+++ src/helpers/getGifts.js
@@ -0,0 +1,14 @@
+export const getGifts = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=9ev22MTaM6JjKEXQUpzJjVJMx5D33UUw&q=${category}&limit=10`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+  
+    const gifts = data.map((img) => ({
+      id: img.id,
+      title: img.title,
+      url: img.images.downsized_medium.url,
+    }));
+  
+    console.log(gifts);
+    return gifts;
+  };
